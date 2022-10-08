119ec81d35ec4dc9c74fc3850d452bb3ef651304
Ahmad-mufied <teranggelap01@gmail.com>
2022-10-08 16:24:46 +0700

    add explanation at 1-Golang-Dasar/8-typeDeclarations.go

--- 1-Golang-Dasar/8-typeDeclarations.go
+++ 1-Golang-Dasar/8-typeDeclarations.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+/*
+Type Declarations
+-- Kemampuan membuat ulang tipe data baru dari tipe data yang sudah ada
+-- iasanya digunakan untuk membuat alias terhadap tipe data yang sudah ada,
+dengan tujuan agar lebih mudah dimengerti
+*/
+
 func main() {
 	type NoKTP string
 	type Married bool
