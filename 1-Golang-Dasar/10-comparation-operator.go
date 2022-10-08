f81106b298462beaf2329608aaf1d149085e8b5b
Ahmad-mufied <teranggelap01@gmail.com>
2022-10-08 17:19:45 +0700

    add explanation at 1-Golang-Dasar/10-comparation-operator.go

--- 1-Golang-Dasar/10-comparation-operator.go
+++ 1-Golang-Dasar/10-comparation-operator.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+/*
+Operasi Perbandingan
+
+-- operasi untuk membandingkan dua buah data
+-- operasi yang menghasilkan nilai boolean (benar atau salah)
+-- benar = true
+-- salah = false
+
+
+Operator Perbandingan
+
+Operator			Keterangan
+>					Lebih dari
+<					Kurang dari
+>=					Lebih dari sama dengan
+<=					Kurang dari sama dengan
+==					sama dengan
+!=					tidak sama dengan
+
+*/
+
 func main() {
 	
 	var name1 = "Ahmad"
